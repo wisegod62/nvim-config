f4576bd8ca201c9310286ac8b9f9001db08d6a65
Theo Cavicchio <107635221+wisegod62@users.noreply.github.com>
2026-07-29 13:32:29 -0400

    Added autocmd to arduino-nvim

--- lua/plugins/arduino.lua
+++ lua/plugins/arduino.lua
@@ -1,12 +1,11 @@
 return {
-  "yuukiflow/Arduino-Nvim",
-  ft = "arduino", -- Only loads when you open an Arduino file
+  "yuukiflow/arduino-nvim",
+  ft = "arduino",
   dependencies = {
-    "nvim-telescope/telescope.nvim", -- Required for interactive dropdown menus
+    "nvim-telescope/telescope.nvim",
     "neovim/nvim-lspconfig",
   },
   init = function()
-    -- Explicitly bind .ino files to the "arduino" filetype so lazy-loading triggers
     vim.filetype.add({
       extension = {
         ino = "arduino",
@@ -15,16 +14,45 @@ return {
     })
   end,
   config = function()
-    -- FIX: The internal module name is 'arduino', NOT 'arduino-nvim'
-    require("arduino").setup({
-      -- You can leave this empty; it will look for 'arduino-cli' in your system PATH
-    })
+    -- Store the plugin instance to read its state variables
+    local arduino = require("arduino")
+    arduino.setup({})
 
     -- Quick Keybindings
     vim.keymap.set("n", "<leader>am", "<cmd>ArduinoSelectModel<cr>", { desc = "Select Arduino Board" })
     vim.keymap.set("n", "<leader>ap", "<cmd>ArduinoSelectPort<cr>", { desc = "Select Serial Port" })
     vim.keymap.set("n", "<leader>au", "<cmd>ArduinoUpload<cr>", { desc = "Compile & Upload Sketch" })
     vim.keymap.set("n", "<leader>as", "<cmd>ArduinoSerial<cr>", { desc = "Open Serial Monitor" })
+
+    -- Automatically handle LSP maps for any board you activate
+    vim.api.nvim_create_autocmd({ "FileType", "User" }, {
+      -- Triggers on file load OR when you update state inside Arduino-Nvim
+      pattern = { "arduino", "ArduinoStateChanged" },
+      callback = function()
+        -- Fall back to Uno if you haven't run <leader>am yet
+        local current_fqbn = arduino.state.fqbn or "arduino:avr:uno"
+        local current_dir = vim.fn.expand("%:p:h")
+        
+        -- Don't run if we aren't in a real workspace directory
+        if current_dir == "" then return end
+
+        -- Silently compile the JSON database in the background
+        local cmd = string.format(
+          "arduino-cli compile --only-compilation-database --fqbn %s %s",
+          current_fqbn,
+          vim.fn.shellescape(current_dir)
+        )
+
+        vim.fn.jobstart(cmd, {
+          on_exit = function(_, exit_code)
+            if exit_code == 0 then
+              -- Reload clangd so it applies the new definitions
+              vim.cmd("LspRestart clangd")
+            end
+          end
+        })
+      end,
+    })
   end,
 }
 
